a7d90dde20c70109abf0f23103f81f10b353c973
jeremiahomueti <jeremiahomueti@gmail.com>
2022-06-29 00:54:41 +0100

    Update

--- app.js
+++ app.js
@@ -2,10 +2,36 @@ const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
+let computerChoice
 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', e => 
 {
     let userChoice = e.target.id 
     userChoiceDisplay.innerHTML = userChoice
+    generateComputerChoice()
 }))
+
+function generateComputerChoice()
+{
+
+    const randomNumber = Math.floor(Math.random() * 3) + 1
+
+    if (randomNumber == 1)
+    {
+        computerChoice = 'Rock'
+    }
+
+    if (randomNumber == 2)
+    {
+        computerChoice = 'Paper'
+    }
+
+    if (randomNumber == 3)
+    {
+        computerChoice = 'Scissors'
+    }
+
+    computerChoiceDisplay.innerHTML = computerChoice
+
+}
