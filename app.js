ba1199b37a2a0a8150a21858de1bab4f9112b757
jeremiahomueti <jeremiahomueti@gmail.com>
2022-06-29 01:17:20 +0100

    Update

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
 const possibleChoices = document.querySelectorAll('button')
 let computerChoice
+let userChoice
 
 
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', e => 
 {
-    let userChoice = e.target.id 
+   userChoice = e.target.id 
     userChoiceDisplay.innerHTML = userChoice
     generateComputerChoice()
+    getResult()
 }))
 
 function generateComputerChoice()
@@ -34,4 +36,44 @@ function generateComputerChoice()
 
     computerChoiceDisplay.innerHTML = computerChoice
 
+}
+
+function getResult() 
+{
+    if (userChoice === computerChoice)
+    {
+        result = "It's a draw!"
+    }
+
+    if(userChoice == 'Rock' && computerChoice == 'Paper')
+    {
+        result = 'Oops! You lost'
+    }
+
+    if(userChoice == 'Rock' && computerChoice == 'Scissors')
+    {
+        result = 'Congrats! You win'
+    }
+
+    if(userChoice == 'Paper' && computerChoice == 'Rock')
+    {
+        result = 'Congrats! You win'
+    }
+
+    if(userChoice == 'Paper' && computerChoice == 'Scissors')
+    {
+        result = 'Oops! You lost'
+    }
+    
+    if(userChoice == 'Scissors' && computerChoice == 'Paper')
+    {
+        result = 'Congrats! You win'
+    }
+    
+    if(userChoice == 'Scissors' && computerChoice == 'Rock')
+    {
+        result = 'Oops! You lost'
+    }
+    
+    resultDisplay.innerHTML = result
 }
